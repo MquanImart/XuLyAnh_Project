8aa0f605fd2f0ae3a2d62679b6321a98829950a2
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-05-11 15:26:08 +0700

    xác định khuôn trang

---
+++ pages/Page_nhan_dang_doi_tuong.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import cv2
+
+def nhan_dang_doi_tuong():
+    st.title("Nhận dạng đối tượng")
+    st.error("Chỉ để test thử")
+    st.sidebar.title("Nhận dạng đối tượng")
+    
+    st.success("Nhập ảnh đối tượng")
+    uploaded_file = st.file_uploader("Chọn ảnh", type=['png', 'jpg', 'jpeg'])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Ảnh gốc', use_column_width=True)
+        st.info("Ảnh gốc")
+        st.image(image, caption='Ảnh gốc', use_column_width=True)
+        st.info("Ảnh xử lí")
+        # chỉ dùng để test chớ chưa xử lí gì cả
+        if st.button("Xử lí"):
+            image = np.array(image)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+            st.image(thresh, caption='Ảnh xử lí', use_column_width=True)
+            st.info("Ảnh xử lí")
+            st.info("Kết quả")
+            if st.button("Nhận dạng"):
+                pass
+    else:
+        st.info("Vui lòng chọn ảnh")
+
+if __name__ == "__main__":
+    nhan_dang_doi_tuong()
