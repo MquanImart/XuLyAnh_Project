10f3a2671440bb57e381d26d705d5e6c84f1eaf3
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-05-12 07:29:04 +0700

    xu li anh

--- pages/Image_Processing.py
+++ pages/Image_Processing.py
@@ -2,27 +2,57 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import cv2
+from xu_ly_anh import Negative, NegativeColor, Logarit, Power, PiecewiseLinear, Histogram, LocalHist, HistStat, MyFilter2D, MySmooth, MyMedianFilter
 
 def xu_li_anh():
-    st.error("Chỉ để test thử")
 
-    st.success("Nhập ảnh")
-    uploaded_file = st.file_uploader("Chọn ảnh", type=['png', 'jpg', 'jpeg'])
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Ảnh gốc', use_column_width=True)
-        st.info("Ảnh gốc")
-        st.image(image, caption='Ảnh gốc', use_column_width=True)
-        st.info("Ảnh xử lí")
-        # chỉ dùng để test chớ chưa xử lí gì cả
-        if st.button("Xử lí"):
-            image = np.array(image)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-            st.image(thresh, caption='Ảnh xử lí', use_column_width=True)
-            st.info("Ảnh xử lí")
-            st.info("Kết quả")
-            if st.button("Nhận dạng"):
-                pass
-    else:
-        st.info("Vui lòng chọn ảnh")
+    imgin = None
+    imgout = None
+    
+    file_uploaded = st.file_uploader("Tải ảnh lên", type=['jpg', 'jpeg', 'png', 'bmp', 'tif'])
+    if file_uploaded is not None:
+        file_bytes = np.asarray(bytearray(file_uploaded.read()), dtype=np.uint8)
+        imgin = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
+        st.image(imgin, channels="GRAY", use_column_width=True, caption="Input Image")
+
+    if imgin is not None:
+        operation = st.selectbox("Các tùy chọn", ["", "Negative", "NegativeColor", "Logarit", "Power",
+                                                              "PiecewiseLinear", "Histogram", "Hist Equal",
+                                                              "Local Hist", "Hist Stat", "Filter 2D", "My Smooth",
+                                                              "Smooth Box", "Smooth Gaussian", "My Median",
+                                                              "Median filter"])
+
+        if operation == "Negative":
+            imgout = Negative(imgin)
+        elif operation == "NegativeColor":
+            imgout = NegativeColor(imgin)
+        elif operation == "Logarit":
+            imgout = Logarit(imgin)
+        elif operation == "Power":
+            imgout = Power(imgin)
+        elif operation == "PiecewiseLinear":
+            imgout = PiecewiseLinear(imgin)
+        elif operation == "Histogram":
+            imgout = Histogram(imgin)
+        elif operation == "Hist Equal":
+            imgout = cv2.equalizeHist(imgin)
+        elif operation == "Local Hist":
+            imgout = LocalHist(imgin)
+        elif operation == "Hist Stat":
+            imgout = HistStat(imgin)
+        elif operation == "Filter 2D":
+            imgout = MyFilter2D(imgin)
+        elif operation == "My Smooth":
+            imgout = MySmooth(imgin)
+        elif operation == "Smooth Box":
+            imgout = cv2.boxFilter(imgin, cv2.CV_8UC1, (45, 45))
+        elif operation == "Smooth Gaussian":
+            imgout = cv2.GaussianBlur(imgin, (43, 43), 7.0)
+        elif operation == "My Median":
+            imgout = MyMedianFilter(imgin)
+        elif operation == "Median filter":
+            imgout = cv2.medianBlur(imgin, 5)
+
+        if imgout is not None:
+            st.image(imgout, channels="GRAY", use_column_width=True, caption="Output Image")
+
